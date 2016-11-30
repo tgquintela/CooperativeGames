5358a9bac98b444a4b5f7b52c635ee2086dba69c
antonio <tgq.spm@gmail.com>
2016-11-30 21:54:31 +0100

    Release information files.

---
+++ CooperativeGames/version.py
@@ -0,0 +1,25 @@
+"""
+Version information for TimeSeriesTools, created during installation by
+setup.py.
+
+Do not add this file to the repository.
+
+"""
+
+import datetime
+
+version = '0.0.0.dev_20161129021829'
+date = 'Tue Nov 29 02:18:29 2016'
+
+# Development version
+dev = True
+
+# Format: (name, major, minor, micro, revision)
+version_info = ('CooperativeGames', '0', '0', '0', None)
+
+# Format: a 'datetime.datetime' instance
+date_info = datetime.datetime(2016, 11, 29, 2, 18, 29, 51663)
+
+# Format: (vcs, vcs_tuple)
+vcs_info = (None, (None, None))
+
